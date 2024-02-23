5f4b079794955a1c526423f1986d5c8d66b00c74
Fürkhtmensch <42923229+Furkhtmensch@users.noreply.github.com>
2024-02-23 22:11:34 +0000

    Update example.c

--- example.c
+++ example.c
@@ -1,6 +1,6 @@
 #include "nprint.h"
 
 int main() {
-    nprint("Let's go %s, only %i days to go before the %.f killers come!", "guys", 10, 3.1416, "Yeah! Let's do this before Mr. %c shows up!", 'K', NULL);
+    nprint("Let's go %s, only %i days to go before the %.f killers come!", "guys", 10, 3.1416, "\nYeah! Let's do this before Mr. %c shows up!", 'K', NULL);
     return 0;
 }
