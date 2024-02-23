122e251e2b5a5b206477ef1749f285216328e910
Fürkhtmensch <42923229+Furkhtmensch@users.noreply.github.com>
2024-02-23 22:05:01 +0000

    Add files via upload

---
+++ example.c
@@ -0,0 +1,6 @@
+#include "nprint.h"
+
+int main() {
+    nprint("Let's go %s, only %i days to go before the %.f killers come!", "guys", 10, 3.1416, "Yeah! Let's do this before Mr. %c shows up!", 'K', NULL);
+    return 0;
+}
